e54b33fea79dadb9a9977ba594635ad2042f657c
JL <jerome.lacaille@gmail.com>
2023-02-02 20:25:33 +0100

    Création du dépot initial.

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .geozone import EARTHDIR
---
+++ geozone.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+GEOZONE - gestion des zones géographiques.
+
+@author: Jérôme Lacaille
+"""
+
+import os
+
+EARTHREP = 'data/zones/_Earth/'
+EARTHDIR = os.path.join(os.path.dirname(__file__),EARTHREP)

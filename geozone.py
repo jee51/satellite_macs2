57cafda7fe6df4402b8356184f2dcef50ea7e5a6
JL <jerome.lacaille@gmail.com>
2023-02-04 19:17:14 +0100

    Cours geozone

--- geozone.py
+++ geozone.py
@@ -9,3 +9,12 @@ import os
 
 EARTHREP = 'data/zones/_Earth/'
 EARTHDIR = os.path.join(os.path.dirname(__file__),EARTHREP)
+
+# -------------------------------------------------------------------------
+def getzone(geolist,i=0):
+    """GETZONE - Récupération des éléments d'intérêt d'une geozone."""
+    
+    ...
+    
+    return zone
+ 
